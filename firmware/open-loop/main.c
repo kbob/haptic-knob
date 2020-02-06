8b23a4e7f76e0ef01dd06cdcb7b56c80b185dbad
kbob <kbob@jogger-egg.com>
2020-02-06 07:03:01 -0500

    improve TIM1 register dump.

--- firmware/open-loop/main.c
+++ firmware/open-loop/main.c
@@ -25,12 +25,14 @@ static void handle_systick(uint32_t millis)
 {
     static uint32_t next_time;
     if (millis >= next_time) {
-        uint32_t idx = (millis % 1000 * CIRCLE_SUBDIVISION) / 1000;
-        int32_t s = sini16(idx);
-        s = s > 0 ? s : 0;                  // rectify
-        uint32_t v = (s * s) >> 14;         // gamma correct
-        v *= timer_cfg.period; v >>= 16;    // scale
-        timer_set_LED_duty(v);
+        if (timer_cfg.enable_LED) {
+            uint32_t idx = (millis % 1000 * CIRCLE_SUBDIVISION) / 1000;
+            int32_t s = sini16(idx);
+            s = s > 0 ? s : 0;                  // rectify
+            uint32_t v = (s * s) >> 14;         // gamma correct
+            v *= timer_cfg.period; v >>= 16;    // scale
+            timer_set_LED_duty(v);
+        }
         next_time += 1;
     }
 }
@@ -46,14 +48,17 @@ int main(void)
     init_systick(rcc_ahb_frequency);
     register_systick_handler(handle_systick);
     init_timer(&timer_cfg);
-    system_millis = 0xFFFFFFFF - 10000;
 
     while (1) {
 
-        printf("CR1 = 0x%lx\n", TIM1_CR1);
-        printf("CR2 = 0x%lx\n", TIM1_CR2);
-        printf("CCMR2 = 0x%lx\n", TIM1_CCMR2);
-        printf("CCR4 = %lu\n", TIM1_CCR4);
+        printf("CR1   %4lx  SR    %4lx  CCER  %4lx  RCR   %4lx  CCR4  %4lx\n",
+           TIM1_CR1,   TIM1_SR,    TIM1_CCER,  TIM1_RCR,   TIM1_CCR4);
+        printf("CR2   %4lx  EGR   %4lx  CNT   %4lx  CCR1  %4lx  BDTR  %4lx\n",
+           TIM1_CR2,   TIM1_EGR,   TIM1_CNT,   TIM1_CCR1,  TIM1_BDTR);
+        printf("SMCR  %4lx  CCMR1 %4lx  PSC   %4lx  CCR2  %4lx  DCR   %4lx\n",
+           TIM1_SMCR,  TIM1_CCMR1, TIM1_PSC,   TIM1_CCR2,  TIM1_DCR);
+        printf("DIER  %4lx  CCMR2 %4lx  ARR   %4lx  CCR3  %4lx  DMAR  %4lx\n",
+           TIM1_DIER,  TIM1_CCMR2, TIM1_ARR,   TIM1_CCR3,  TIM1_DMAR);
         printf("\n");
 
         for (int i = 0; i < 20000000; i++)
